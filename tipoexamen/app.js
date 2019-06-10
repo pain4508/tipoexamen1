9291d958fadb43372ce774d7376ddaf249c5f46d
MOISES <kmsprado2546@gmail.com>
2019-06-09 21:18:54 -0600

    Implementacion de Get All y de Post New

--- tipoexamen/app.js
+++ tipoexamen/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-
 var apiRouter = require('./routes/api/api');
 
 var app = express();
--- tipoexamen/routes/api/personas.js
+++ tipoexamen/routes/api/personas.js
@@ -1,5 +1,8 @@
+
 var express = require("express");
 var router = express.Router();
+//Esto es nuevo para generar un identificador unico
+var uuid = require('uuid/v4');
 
 var personaCollection = [];
 var personaStruct = {
@@ -11,16 +14,39 @@ var personaStruct = {
     nota: ''
 }
 
+personaCollection.push(
+    Object.assign(
+        {},
+        personaStruct,
+        {
+            id:uuid(),
+            nombre: 'Cristian Prado',
+            correo: 'kmsprado2546@gmail.com',
+            telefono: '99000000',
+            nota: 'Ejemplo de Persona ejercicio tipo examen'
+        }
+    )
+
+);
+
 router.get('/', (req, res, next)=>{
- res.status(403).json({msg:"Not Imprement"});
+    //Obtiene la coleccion de personas
+ res.status(200).json(personaCollection);
 });//get
 
 router.get('/:id', (req, res, next)=>{
-    res.status(403).json({msg:"Not Imprement"});
+    res.status(200).json({msg:"Not Imprement"});
     }); //get one by Id
 
 router.post('/',(req, res, next)=>{
-    res.status(403).json({msg:"Not Imprement"});
+    var newPersona = Object.assign(
+        {},
+        personaStruct,
+        {id:uuid()},
+        req.body
+    );
+    personaCollection.push(newPersona);
+    res.status(200).json(newPersona);
 
 });//post.
 
